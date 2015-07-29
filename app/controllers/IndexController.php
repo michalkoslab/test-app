bcbb8e385684b66b2a36ccc4c9de937ca4c379da
Michał Zydroń <michal.zydron@deslay.pl>
2015-07-29 18:41:48 +0200

    Add comments

--- app/controllers/IndexController.php
+++ app/controllers/IndexController.php
@@ -6,14 +6,30 @@ use TestApp\Components\VarnishLogReader;
 use TestApp\Components\RssReader;
 use TestApp\Components\JsonReader;
 
+/**
+ * Class IndexController
+ *
+ * Main application controller.
+ *
+ * @package TestApp\Controllers
+ * @author Michał Zydroń <michal.zydron@gmail.com>
+ */
 class IndexController extends Controller
 {
+    /**
+     *  Redirects to "varnish-log" route.
+     */
     public function indexAction()
     {
         $this->view->disable();
         $this->response->redirect('varnish-log');
     }
 
+    /**
+     * Generates view with data from Varnish log file.
+     *
+     * @throws \Exception If the resource doesn't exist.
+     */
     public function varnishLogAction()
     {
         $varnishLog = new VarnishLogReader('../app/resources/varnish.log');
@@ -23,6 +39,11 @@ class IndexController extends Controller
         $this->view->files = $varnishLog->getTheMostRequestedFiles(5);
     }
 
+    /**
+     * Generates view with data from RSS-feed.
+     *
+     * @throws \Exception If the resource doesn't exist.
+     */
     public function rssFeedAction()
     {
         $rss = new RssReader('http://www.vg.no/rss/nyfront.php?frontId=1');
@@ -32,6 +53,11 @@ class IndexController extends Controller
         $this->view->articles = $rss->getData();
     }
 
+    /**
+     * Generates view with data from JSON-feed.
+     *
+     * @throws \Exception If the resource doesn't exist.
+     */
     public function jsonFeedAction()
     {
         $json = new JsonReader('http://rexxars.com/playground/testfeed/');
